f9b720e7a86455121ca69b28e91ccc99a10471eb
Mikhail Barulin <barulin.ma@gmail.com>
2025-03-08 00:25:12 +0300

    feat: add shadcn components and user them

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,8 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  rootDir: "./src",
+  srcDir: "src/",
   compatibilityDate: "2024-11-01",
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   devtools: { enabled: true },
   devServer: {
     port: 3002,
